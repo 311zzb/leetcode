c2dc63bc6c61084ce73965f076df81af3def7a10
311zzb <shimakazegumo@gmail.com>
2022-09-09 15:20:10 +0900

    two-sum Brute-force solution

--- 1.two-sum_brute.java
+++ 1.two-sum_brute.java
@@ -7,6 +7,7 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // Double loop
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
