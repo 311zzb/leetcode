bfb72a30ff2aa3a1429cc1d4bf681d998f49ab87
311zzb <shimakazegumo@gmail.com>
2022-09-14 23:38:42 +0900

    121.best-time-to-buy-and-sell-stock Brute Force solution

---
+++ 121.best-time-to-buy-and-sell-stock_brute.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=121 lang=java
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
+// @lc code=end
+
