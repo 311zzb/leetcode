383a263ae0159c72d19f2ad2afb2ac1c41b21d04
311zzb <shimakazegumo@gmail.com>
2022-09-24 09:51:50 +0900

    424.longest-repeating-character-replacement Sliding Window Simple solution

---
+++ 424.longest-repeating-character-replacement_slidingWindowSimple.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=424 lang=java
+ *
+ * [424] Longest Repeating Character Replacement
+ */
+
+// @lc code=start
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int len = s.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+        for (int end = 0; end < len; end++) {
+            maxCount = Math.max(maxCount, ++count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A'] -= 1;
+                start += 1;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+}
+// @lc code=end
+
